e1095692494f485b01202e748e77972b03646b9a
P-JSL <althsuwpfl@gmail.com>
2023-08-14 20:37:55 +0900

    Chapter3, loop and Iteration , Casting and Convert

---
+++ CastingConverting/Program.cs
@@ -0,0 +1,83 @@
+﻿using static System.Console;
+using static System.Convert;
+
+int a = 10;
+double b = a;
+WriteLine(b);
+
+double c = 9.8;
+int d = (int)c;
+WriteLine(d);
+
+long e = 10;
+int f = (int)e;
+WriteLine($"e is {e:N0} and f is {f:N0}");
+e = long.MaxValue;
+f = (int)e;
+WriteLine($"e is {e:N0} and f is {f:N0}");
+
+e = 5_000_000_000;
+f = (int)e;
+WriteLine($"e is {e:N0} and f is {f:N0}");
+
+double g = 9.8;
+int h = ToInt32(g);
+WriteLine($"g is {g:N} and h is {h:N}");
+
+//반올림
+double[] doubles = new[]
+{
+    9.49, 9.5, 9.51, 10.49, 10.5, 10.51
+};
+foreach( double n in doubles)
+{
+    WriteLine($"ToInt({n}) is {ToInt32(n)}");
+}
+
+foreach(double n in doubles)
+{
+    WriteLine(
+        format: "Math.Round({0}), 0, MidpointRounding.AwayFromZero is  {1}",
+        arg0: n,
+        arg1: Math.Round(value: n, digits: 0, mode: MidpointRounding.AwayFromZero)
+        );
+}
+
+//바이너리 객체 -> string 변환
+//ToBase64String 및 FormBase64String
+
+//128bit array
+byte[] binaryObject = new byte[128];
+
+//무작위
+(new Random()).NextBytes(binaryObject);
+WriteLine("Binary Object as bytes : ");
+for(int index = 0; index < binaryObject.Length; index++)
+{
+    Write($"{binaryObject[index]:X}");
+}
+WriteLine();
+
+string encoded = Convert.ToBase64String(binaryObject);
+WriteLine($"binary Ojbect as Base64 : {encoded}");
+
+//string -> number, date, time
+
+int age = int.Parse("27");
+DateTime birthday = DateTime.Parse("4 July 1980");
+WriteLine($"I was born {age} years ago.");
+WriteLine($"My Birthday is {birthday}");
+WriteLine($"My Birthday is {birthday:D}");
+
+//tryParse
+WriteLine("How many eggs are there?");
+int count;
+string input = ReadLine();
+if (int.TryParse(input, out count))
+{
+    WriteLine($"There are {count} eggs.");
+}
+else
+{
+    WriteLine("I could not parse  the input.");
+}
--- IterationStatments/Program.cs
+++ IterationStatments/Program.cs
@@ -1,4 +1,5 @@
-﻿using static System.Console;
+﻿using System.Collections;
+using static System.Console;
 
 int x = 0;
 while(x < 10)
@@ -11,7 +12,7 @@ while(x < 10)
 
 string? password;
 int count = 0;
-
+/*
 do {
     Write("Enter your password : ");
     password = ReadLine();
@@ -24,6 +25,26 @@ do {
         WriteLine("10 times fail your password");
     }
 } while (password != "Pa$$w0rd");
-WriteLine("correct");
+WriteLine("correct");*/
 
 //for loop
+for(int y=0; y<10; y++)
+{
+    WriteLine(y);
+}
+
+//foreach loop
+// 배열이나 컬렉션 내의 각 아이템에 대해 코드블록 실행
+//example
+string[] names = { "Adam","Barry","Charlie"};
+foreach(string name in names)
+{
+    WriteLine($"{name} has {name.Length} characters");
+}
+
+IEnumerator e = names.GetEnumerator();
+while (e.MoveNext())
+{
+    string name = (string)e.Current;
+    WriteLine($"{name} has {name.Length} charactor");
+}
