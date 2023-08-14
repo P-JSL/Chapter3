da7242e11cdde8acd47dabc5b04b0d0a4ca333dc
P-JSL <althsuwpfl@gmail.com>
2023-08-14 17:08:18 +0900

    초기 커밋.

---
+++ BitwiseAndShiftOperators/Program.cs
@@ -0,0 +1,10 @@
+﻿using static System.Console;
+
+int a = 10; // 0000 1010
+int b = 6; // 0000 0110;
+
+WriteLine($"a = {a}");
+WriteLine($"b = {b}");
+WriteLine($"a & b = {a & b}");
+WriteLine($"a | b = {a | b}");
+WriteLine($"a ^ b = {a ^ b}");
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using static System.Console;
+
+int a = 3;
+int b = a++;
+Console.WriteLine($"a is {a}, b is {b }");
+
+int c = 3;
+int d = ++c;
+Console.WriteLine($"c is {c}, d is {d}");
+
+//이항산술
+
+int e = 11;
+int f = 3;
+WriteLine($"e is {e}, f is {f}");
+WriteLine($"e + f = {e + f}");
+WriteLine($"e - f = {e - f}");
+WriteLine($"e * f = {e * f}");
+WriteLine($"e / f = {e / f}");
+WriteLine($"e % f = {e % f}");
+
+double g = 11.0;
+WriteLine($"g is {g:N1}, f is {f}");
+WriteLine($"g / f = {g/f}");
+
+//대입연산자
+int p = 6;
+p += 3;
+p -= 3;
+p *= 3;
+p /= 3;
+
