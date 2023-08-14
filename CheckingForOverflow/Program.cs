cece290363976f42af47157e75f11916e40cde67
P-JSL <althsuwpfl@gmail.com>
2023-08-14 21:38:45 +0900

    3장 끝

---
+++ CheckingForOverflow/Program.cs
@@ -0,0 +1,30 @@
+﻿using static System.Console;
+
+checked
+{
+    try
+    {
+        int x = int.MaxValue - 1;
+        WriteLine($"Initial value : {x}");
+        x++;
+        WriteLine($"After incrementing : {x}");
+        x++;
+        WriteLine($"After incrementing : {x}");
+        x++;
+        WriteLine($"After incrementing : {x}");
+    } catch (OverflowException)
+    {
+        WriteLine("오버플로 예외가 발생했지만 여기서 처리했음.");
+    }
+}
+unchecked
+{
+    int y = int.MaxValue + 1;
+    WriteLine($"Initial value : {y}");
+    y--;
+    WriteLine($"After incrementing : {y}");
+    y--;
+    WriteLine($"After incrementing : {y}");
+    y--;
+    WriteLine($"After incrementing : {y}");
+}
---
+++ HandlingException/Program.cs
@@ -0,0 +1,37 @@
+﻿using static System.Console;
+Write("Before pasing\nWhat is your age? ");
+string input = ReadLine();
+try
+{
+    int age = int.Parse(input);
+    WriteLine($"You are {age} years old.");
+}
+catch(FormatException ex )
+{
+    WriteLine("The age you entered is not a vaild number format.");
+}
+catch (OverflowException)
+{
+    WriteLine("Your age is a vaild number foramt but it is either too big or samll");
+}
+catch(Exception ex)
+{
+    Write($"{ex.GetType} says {ex.Message}");
+}
+WriteLine("After pasing");
+
+
+Write("Enter the amount : ");
+string? amount = ReadLine();
+try
+{
+    decimal amountValue = decimal.Parse(amount);
+}
+catch(FormatException) when (amount.Contains("$"))
+{
+    WriteLine("Amounts cannot use the dollar sign!");
+}
+catch (FormatException)
+{
+    WriteLine("Amounts must only contain digits!.");
+}
